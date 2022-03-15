2e7375259ade0fe059351556830798e87cccb8a4
Nurlan <78497823+nurl8n@users.noreply.github.com>
2022-03-15 22:36:37 +0600

    Create Main.java

---
+++ 6/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+//        Computer comp = new Computer("Linux", 512, 512, 3);
+///*
+//        comp.setName("Linux");
+//        comp.setRam(1024);
+//*/
+//        comp.on();
+//        comp.load();
+//        comp.off();
+
+        Laptop upComputer = new Laptop();
+
+        upComputer.on();
+        upComputer.load();
+        upComputer.off();
+    }
+}
+
